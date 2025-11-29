a5783014f5f48b54ae103d8be741418f1e35ddb9
megvadulthangya <67601373+megvadulthangya@users.noreply.github.com>
2025-11-29 12:25:27 +0100

    Something
    :(

--- src/ign-api/run-worker.sh
+++ src/ign-api/run-worker.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-echo "🚀 [Launcher] Starting Infinite Worker Loop..."
-
-while true; do
-    echo "🔄 [Worker] Starting python worker..."
-    
-    # Elindítjuk a workert
-    python worker.py
-    
-    # Ha a python folyamat kilép (mert kész, vagy mert meghalt), ide jutunk
-    EXIT_CODE=$?
-    echo "⚠️ [Worker] Stopped/Crashed with exit code: $EXIT_CODE. Restarting in 2 seconds..."
-    
-    # Várunk kicsit, hogy ne pörgesse szét a CPU-t hiba esetén
-    sleep 2
-done
+python worker.py
